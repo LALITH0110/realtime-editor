5052432025abdcc31173e70ffe435fa32519f027
LALITH0110 <lalith.kothuru@gmail.com>
2025-07-12 19:19:49 +0530

    Create LoginRequest.java

---
+++ backend/src/main/java/com/example/demo/dto/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+
+/**
+ * DTO for user login requests
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+} 
