3900d1e83495f95a48d157cb08d616348c311e5b
LALITH0110 <lalith.kothuru@gmail.com>
2025-07-12 19:19:53 +0530

    Create SignupRequest.java

---
+++ backend/src/main/java/com/example/demo/dto/SignupRequest.java
@@ -0,0 +1,32 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+/**
+ * DTO for user signup requests
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    private String password;
+} 
